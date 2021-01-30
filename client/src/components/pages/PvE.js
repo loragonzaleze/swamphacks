4bd02f06eb50b30245a16da6db29b6c2029e7311
Naseem Hamed <naseemhamed100@gmail.com>
2021-01-30 16:48:57 -0500

    phaser base

--- client/src/components/pages/PvE.js
+++ client/src/components/pages/PvE.js
@@ -1,11 +1,44 @@
 import React, { Component } from "react";
 import Phaser from "phaser";
 import {IonPhaser} from "@ion-phaser/react";
-import background from "./gameImages/background.png"
-import gator from "./gameImages/Albert1.png"
-import platform from "./gameImages/platform.png"
 
 class PvE extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      initialize: true,
+      game: {
+        width: "100%",
+        height: "100%",
+        type: Phaser.AUTO,
+        scene: {
+          init:
+            function() {
+              this.cameras.main.setBackgroundColor('#24252A')
+
+            },
+          preload: function() {
+            console.log("first")
+            this.load.image('sky', "./background.png")
+            this.load.image('albert', "./Albert1.png")
+            this.load.image('ground', "./platform.png")
+          },
+          create:function() {
+            console.log("here")
+            //this.game.physics.();
+            this.add.sprite(400,400, 'sky');
+            this.add.sprite(300,100, 'ground');
+            this.add.sprite(100,100, 'ground');
+          },
+          update:
+            function() {
+            }
+        }
+      }
+    }
+
+  }
 
   _preload =() =>
   {
@@ -32,33 +65,6 @@ class PvE extends Component {
 
   }
 
-  state = {
-    initialize: true,
-    game: {
-      width: "100%",
-      height: "100%",
-      type: Phaser.AUTO,
-      scene: {
-        init:
-        function() {
-          this.cameras.main.setBackgroundColor('#24252A')
-          this.load.image('sky', background)
-          this.load.image('gator', gator)
-          this.load.image('ground', platform)
-        },
-         create:function() {
-           console.log("here")
-           //this.game.physics.();
-           let img = this.add.sprite(100,100, 'sky');
-           img.scale.setTo(5,5);
-         },
-        update: this._update,
-          //function() {
-          //this.helloWorld.angle += 1;
-        //}
-      }
-    }
-  }
 // function() {
 //   this.helloWorld = this.add.text(
 //     this.cameras.main.centerX,
@@ -87,6 +93,7 @@ class PvE extends Component {
 
   render() {
     const { initialize, game } = this.state
+    console.log('Last')
     return (
       <IonPhaser game={game} initialize={initialize} />
     )
