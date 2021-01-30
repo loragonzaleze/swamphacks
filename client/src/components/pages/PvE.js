9f68a750e231d23cf571df2035168ab0e60c65ee
unknown <brucema7@gmail.com>
2021-01-30 18:19:34 -0500

    PvE changes

--- client/src/components/pages/PvE.js
+++ client/src/components/pages/PvE.js
@@ -8,7 +8,12 @@ class PvE extends Component {
 
     this.state = {
       initialize: true,
+
       game: {
+        physics: {
+          arcade: {
+            debug: true,
+          }},
         width: "100%",
         height: "100%",
         type: Phaser.AUTO,
@@ -21,19 +26,46 @@ class PvE extends Component {
           preload: function() {
             console.log("first")
             this.load.image('sky', "./background.png")
-            this.load.image('albert', "./Albert1.png")
+            this.load.image('marston', "./background2.png")
+            this.load.image('albert', "./idle.png")
             this.load.image('ground', "./platform.png")
           },
           create:function() {
-            console.log("here")
+            this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+            this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+            this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+            this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
             let windowWidth = window.innerWidth;
             let windowHeight = window.innerHeight;
-            this.bg = this.add.image(windowWidth / 2, windowHeight / 2, 'sky');
+            this.bg = this.add.image(windowWidth / 2, windowHeight / 2, 'marston');
             this.bg.setDisplaySize(windowWidth, windowHeight);
             this.add.sprite(64, windowHeight-25, "ground");
+            this.albert =this.physics.add.sprite(this.cameras.main.centerX,100, 'albert')
+
+
+
+
           },
           update:
             function() {
+              if(this.keyD.isDown){
+                this.albert.x += 4;
+                console.log("PRESSINg Right")
+              }
+              if(this.keyA.isDown){
+                this.albert.x -= 4;
+                console.log("PRESSINg Right")
+              }
+              if(this.keyS.isDown){
+                this.albert.y += 4;
+                console.log("PRESSINg Right")
+              }
+              if(this.keyW.isDown){
+                this.albert.y -= 4;
+                console.log("PRESSINg Right")
+              }
+
             }
         }
       }
