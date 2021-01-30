57862a35ae511098e00251998a78b6a8c359c2db
Samir Ziad <samirziad32@gmail.com>
2021-01-30 17:40:26 -0500

    yes

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import background from './assets/maxresdefault.jpg'
 import SettingsIcon from './assets/settings-solid.png'
 import "./HomePage.css"
-import {AwesomeButton} from "react-awesome-button";
 import "react-awesome-button/dist/styles.css";
 
 
@@ -22,6 +21,10 @@ class HomePage extends Component {
       user:2
     })
   }
+  myfunction()
+  {
+    console.log("CLICKED");
+  }
 
   render() {
     return (
@@ -30,25 +33,6 @@ class HomePage extends Component {
           Rumble in the Swamp!
         </div>
 
-        <AwesomeButton  type="primary"
-                        style = {{position:this.fixed, left: 680, top: 300}}
-                       onPress={() =>
-                       {}
-                       }
-        >Play</AwesomeButton>
-        <AwesomeButton type="primary"
-                        style = {{position:this.fixed, left: 0, top: 400, backgroundImage: `url(${SettingsIcon})`}}
-                        onPress={() =>
-                        {}
-                        }
-        > </AwesomeButton>
-
-        <AwesomeButton type="primary"
-                              style = {{position:this.fixed, left: 1050, top: 400 }}
-                              onPress={() =>
-                              {}
-                              }
-        >?</AwesomeButton>
       </div>
     );
   }
