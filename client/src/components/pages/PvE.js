f2a83eef86802721bb175026f0f62a50d9690d26
Naseem Hamed <naseemhamed100@gmail.com>
2021-01-30 17:21:56 -0500

    phaser base

--- client/src/components/pages/PvE.js
+++ client/src/components/pages/PvE.js
@@ -41,56 +41,6 @@ class PvE extends Component {
 
   }
 
-  _preload =() =>
-  {
-    this.cameras.main.setBackgroundColor('#24252A')
-    this.load.image('sky', background)
-    this.load.image('gator', gator)
-    this.load.image('ground', platform)
-  }
-  _create =() =>
-  {
-    console.log("here")
-    //this.game.physics.();
-    this.add.sprite(0,0, 'sky')
-    let platforms = this.game.adxd.group()
-    platforms.enableBody = true
-
-    let ground = platforms.create(0, this.game.world.height - 64, 'ground')
-    ground.scale.setTo(2,2)
-    ground.body.immovable = true
-
-  }
-  _update =() =>
-  {
-
-  }
-
-// function() {
-//   this.helloWorld = this.add.text(
-//     this.cameras.main.centerX,
-//     this.cameras.main.centerY,
-//     "Hello World", {
-//       font: "40px Arial",
-//       fill: "#ffffff"
-//     }
-//   );
-//   this.helloWorld.setOrigin(0.5);
-// },
-
-  componentDidMount() {
-    // this.gamePvE = new Phaser.Game({
-    //   type: Phaser.AUTO,
-    //   width: 800,
-    //   height: 600,
-    //   parent: "gameId",
-    //   scene: {
-    //     preload: this._preload,
-    //     create: this._create,
-    //     update: this._update
-    //   }
-    // })
-  }
 
   render() {
     const { initialize, game } = this.state
